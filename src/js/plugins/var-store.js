aa6b8ad2648176446f9cbb3eab5116d217d572e7
Todor Imreorov <blurymind@gmail.com>
2021-06-07 15:55:03 +0100

    add json editor to variables editor plugin

--- src/js/plugins/var-store.js
+++ src/js/plugins/var-store.js
@@ -1,46 +1,52 @@
+const JSONEditor = require('./jsoneditor/dist/jsoneditor-minimalist.min');
+
 export var VarStore = function({
   app,
   createButton,
   setPluginStore,
   getPluginStore,
 }) {
-  this.localVariables = {};
   const self = this;
 
-  // Build the interface here
+  this.onOpenDialog = async () => {
+    let editor = null;
+    const { value: formValues } = await Swal.fire({
+      title: 'Playtest starting variables',
+      html: `<div id="jsoneditor">
+              </div>`,
+      focusConfirm: false,
+      onOpen: () => {
+        // create the editor
+        const container = document.getElementById('jsoneditor');
+        const options = {};
+        editor = new JSONEditor(container, options);
+        require('./jsoneditor/dist/jsoneditor.min.css');
+        require('./jsoneditor/size-overrides.css');
+        console.log(app.settings.theme());
+        if (app.settings.theme() === 'dracula') {
+          require('./jsoneditor/json-editor-darktheme.css');
+        }
+
+        const localVariables = getPluginStore(self);
+        // set json
+        editor.set(localVariables.fields);
+      },
+      preConfirm: () => {
+        return editor.get();
+      },
+    });
+
+    if (formValues) {
+      setPluginStore(self, 'fields', formValues);
+    }
+  };
+
   this.onload = () => {
-    self.localVariables = getPluginStore(self);
-
-    console.log(self.localVariables);
-    this.onOpenDialog = async () => {
-      const { value: formValues } = await Swal.fire({
-        title: 'Multiple inputs',
-        html: `<input id="swal-input1" class="swal2-input" value="${self.localVariables.fields[0]}"> 
-          <input id="swal-input2" class="swal2-input">`,
-        focusConfirm: false,
-        preConfirm: () => {
-          return [
-            document.getElementById('swal-input1').value,
-            document.getElementById('swal-input2').value,
-          ];
-        },
-      });
-
-      console.log('FORM', formValues);
-      if (formValues) {
-        setPluginStore(self, 'fields', formValues);
-      }
-    };
-
     // create a button in the file menu
-    const dialogButton = createButton(self, {
+    createButton(self, {
       name: 'Variables',
       attachTo: 'fileMenuDropdown',
       onClick: 'onOpenDialog',
     });
-
-    console.log('yay', app.plugins);
-    // create a dialog to use with the button
-    // createDialog(self, { name: 'Variables' });
   };
 };
