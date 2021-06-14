23a6882d5a5260cf2ace094acddcba52838bc272
Todor Imreorov <blurymind@gmail.com>
2021-06-14 18:38:45 +0100

    fix service worker path shouldnt be absolute

--- src/sw-src.js
+++ src/sw-src.js
@@ -2,6 +2,6 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { NetworkFirst } from 'workbox-strategies';
 import { registerRoute } from 'workbox-routing';
 
-console.log('Yarn\'s service worker is caching files');
-registerRoute(/https:\/\/yarnspinnertool\.github\.io\/YarnEditor\//, new NetworkFirst());
+console.log("Yarn's service worker is caching files");
+registerRoute(/\.\/YarnEditor\//, new NetworkFirst());
 precacheAndRoute(self.__WB_MANIFEST);
