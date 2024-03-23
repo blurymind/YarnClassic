b6a8518bfaea16856e6bfb47282766983ad31a4b
blurymind <blurymind@gmail.com>
2024-03-23 13:15:35 +0000

    Updates

--- public/plugins/jsoneditor/jsoneditor.js
+++ public/plugins/jsoneditor/jsoneditor.js
@@ -1,51 +1,61 @@
-export var JSONEditor = function({id}) {
-    this.tableAdd = ({key, value}) =>{
-        var $clone = this.table.find('tr.hide').clone(true).removeClass('hide table-line');
-        if(key && value) {
-            console.log("mutate", {key, value, html:$clone.html()})
-            $clone.html(`
+export var JSONEditor = function({ id }) {
+  this.tableAdd = ({ key, value }) => {
+    var $clone = this.table
+      .find('tr.hide')
+      .clone(true)
+      .removeClass('hide table-line');
+    if (key && value) {
+      console.log('mutate', { key, value, html: $clone.html() });
+      $clone.html(`
                 <td contenteditable="true" class="cell">${key}</td>
                 <td contenteditable="true" class="cell">${value}</td>
                 <td>
                     <span class="table-remove glyphicon glyphicon-remove">x delete</span>
                 </td>
-            `)
-            $('.table-remove').on("click",function() {
-                $(this).parents('tr').detach();
-            });
-    
-        }
-        this.table.find('table').append($clone);
+            `);
+      $('.table-remove').on('click', function() {
+        $(this)
+          .parents('tr')
+          .detach();
+      });
     }
-    this.getValue = () => {
-        var $rows = this.table.find('tr:not(:hidden)');
-        var headers = [];
-        var data = [];
+    this.table.find('table').append($clone);
+  };
+  this.getValue = () => {
+    var $rows = this.table.find('tr:not(:hidden)');
+    var headers = [];
+    var data = [];
 
-        // Get the headers (add special header logic here)
-        $($rows.shift()).find('th:not(:empty):not([data-attr-ignore])').each(function() {
-            headers.push($(this).text().toLowerCase());
-        });
+    // Get the headers (add special header logic here)
+    $($rows.shift())
+      .find('th:not(:empty):not([data-attr-ignore])')
+      .each(function() {
+        headers.push(
+          $(this)
+            .text()
+            .toLowerCase()
+        );
+      });
 
-        // Turn all existing rows into a loopable array
-        $rows.each(function() {
-            var $td = $(this).find('td');
-            var h = {};
-            // Use the headers from earlier to name our hash keys
-            headers.forEach(function(header, i) {
-                h[header] = $td.eq(i).text(); // will adapt for inputs if text is empty
-            });
+    // Turn all existing rows into a loopable array
+    $rows.each(function() {
+      var $td = $(this).find('td');
+      var h = {};
+      // Use the headers from earlier to name our hash keys
+      headers.forEach(function(header, i) {
+        h[header] = $td.eq(i).text(); // will adapt for inputs if text is empty
+      });
 
-            data.push(h);
-        });
-        // Output the result
-        // var $EXPORT = $('#save');
-        // $EXPORT.text(JSON.stringify(data));
-        return data;;
-    }
+      data.push(h);
+    });
+    // Output the result
+    // var $EXPORT = $('#save');
+    // $EXPORT.text(JSON.stringify(data));
+    return data;
+  };
 
-    this.init = () => {
-        document.getElementById(id).innerHTML = `
+  this.init = () => {
+    document.getElementById(id).innerHTML = `
         <div class="container">
   <div id="table" class="table-editable">
     <table class="table">
@@ -73,29 +83,31 @@ export var JSONEditor = function({id}) {
     </div>
   </div>
 </div>
-        `
-        var $TABLE = $('#table');
-        var $BTN = $('#save-btn');
-        this.table = $TABLE;
-        this.rows = $TABLE.find('tr:not(:hidden)');
-        var $EXPORT = $('#save-btn');
-        $EXPORT.addClass("hide");
+        `;
+    var $TABLE = $('#table');
+    var $BTN = $('#save-btn');
+    this.table = $TABLE;
+    this.rows = $TABLE.find('tr:not(:hidden)');
+    var $EXPORT = $('#save-btn');
+    $EXPORT.addClass('hide');
 
-        $('.table-add').on("click", this.tableAdd);
+    $('.table-add').on('click', this.tableAdd);
 
-        $('.table-remove').on("click",function() {
-            console.log({removeThis: this})
-            $(this).parents('tr').detach();
-        });
+    $('.table-remove').on('click', function() {
+      console.log({ removeThis: this });
+      $(this)
+        .parents('tr')
+        .detach();
+    });
 
-        // A few jQuery helpers for exporting only
-        jQuery.fn.pop = [].pop;
-        jQuery.fn.shift = [].shift;
-        $BTN.on("click",this.getValue);
-    }
-    this.init();
+    // A few jQuery helpers for exporting only
+    jQuery.fn.pop = [].pop;
+    jQuery.fn.shift = [].shift;
+    $BTN.on('click', this.getValue);
+  };
+  this.init();
 
-    this.setValue =(newValue = []) =>{
-        newValue.forEach(this.tableAdd);
-    }
-}
+  this.setValue = (newValue = []) => {
+    newValue.forEach(this.tableAdd);
+  };
+};
