7aefa23f8840dcd69c370fa8492f6243a8e0fc28
blurymind <blurymind@gmail.com>
2024-03-25 18:17:16 +0000

    disable preview formaters

--- src/js/classes/richTextFormatter.js
+++ src/js/classes/richTextFormatter.js
@@ -10,33 +10,38 @@ export const RichTextFormatter = function(app) {
     const ytRegex = /(?:http(?:s?):\/\/|)(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?(?:\?t=[0-9]+)?/gi;
     const otherUrlPattern = `^(?!${twRegex.source}|${ytRegex.source}|${instaRegex.source})https?:.*$`;
     const combinedRegex = new RegExp(otherUrlPattern, 'gm');
+    // result = result.replace(combinedRegex, function(id) {
+    //   return `
+    //    <div style="position: relative">
+    //     <iframe src="${id}" style="position:relative;left:-70px !important;width:90%;height:500px"></iframe>
+    //    </div>
+    // `;
+    // });
     result = result.replace(combinedRegex, function(id) {
       return `
-       <div style="position: relative">
-        <iframe src="${id}" style="position:relative;left:-70px !important;width:90%;height:500px"></iframe>
-       </div>
+      <a class="link" href="${id}">${id}</a>
     `;
     });
     // add tweet embeds :3
-    const tweets = [];
-    result = result.replace(twRegex, function(id) {
-      const extractedtweetId = id.match(
-        /https:\/\/twitter.com\/.*\/status\/([0-9]+)/i
-      );
-      if (extractedtweetId.length > 1) {
-        tweets.push(extractedtweetId[1]);
-        return `<a class="tweet" id="${extractedtweetId[1]}"></a>`;
-      }
-    });
-    setTimeout(() => {
-      const tweetItems = document.querySelectorAll('.tweet');
-      tweets.forEach((tweetPost, index) => {
-        twttr.widgets.createTweet(tweetPost, tweetItems[index], {
-          align: 'center',
-          follow: false,
-        });
-      });
-    }, 500);
+    // const tweets = [];
+    // result = result.replace(twRegex, function(id) {
+    //   const extractedtweetId = id.match(
+    //     /https:\/\/twitter.com\/.*\/status\/([0-9]+)/i
+    //   );
+    //   if (extractedtweetId.length > 1) {
+    //     tweets.push(extractedtweetId[1]);
+    //     return `<a class="tweet" id="${extractedtweetId[1]}"></a>`;
+    //   }
+    // });
+    // setTimeout(() => {
+    //   const tweetItems = document.querySelectorAll('.tweet');
+    //   tweets.forEach((tweetPost, index) => {
+    //     twttr.widgets.createTweet(tweetPost, tweetItems[index], {
+    //       align: 'center',
+    //       follow: false,
+    //     });
+    //   });
+    // }, 500);
     // create Youtube previews :)
     result = result.replace(ytRegex, function(id) {
       const extractedId = id.match(
